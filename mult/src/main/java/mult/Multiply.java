7500f400885e4a8cb1ac45c5c3d9450f33581db0
ajay naveen <ajay naveen@192.168.0.5>
2021-05-21 16:47:39 +0530

    multiplying 2 numbers

---
+++ mult/src/main/java/mult/Multiply.java
@@ -0,0 +1,12 @@
+package mult;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		
+		int num1=6,num2=81,product;
+		product=num1*num2;
+		System.out.println("The product of given two numbers is: "+product);
+	}
+
+}
